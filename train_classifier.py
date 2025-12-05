fd55eb238cd71cd26f3873f8f78e849cb79fe3fb
dq2024 <dq2024@nyu.edu>
2025-12-05 14:37:13 -0600

    first commit

--- train_classifier.py
+++ train_classifier.py
@@ -200,23 +200,24 @@ def main():
     MODEL_NAME = "meta-llama/Llama-3.2-1B"
     TRAIN_FILE = "/scratch/dq2024/doc-verifier/verifier_training_data/train_llama_query_split.jsonl"
     VAL_FILE = "/scratch/dq2024/doc-verifier/verifier_training_data/val_llama_query_split.jsonl"
-    OUTPUT_DIR = "/scratch/dq2024/doc-verifier/models/llama-3.2-1b-verifier-classifier-query_split-do0.5-lorado0.2"
+    OUTPUT_DIR = "/scratch/dq2024/doc-verifier/models/llama-3.2-1b-verifier-classifier-query_split_lr5e-6"
     
     HF_TOKEN = os.environ.get('HF_TOKEN', None)
     
     # Hyperparameters
     BATCH_SIZE = 8
     GRAD_ACCUM_STEPS = 2
-    LEARNING_RATE = 2e-5
+    #LEARNING_RATE = 2e-5
+    LEARNING_RATE=5e-6
     NUM_EPOCHS = 10
     MAX_LENGTH = 1024  # Shorter since we don't need to generate
     PATIENCE = 3
-    CLASSIFIER_DROPOUT = 0.5
+    CLASSIFIER_DROPOUT = 0.3
     
     # LoRA config
     LORA_R = 8
     LORA_ALPHA = 16
-    LORA_DROPOUT = 0.2
+    LORA_DROPOUT = 0.1
     
     if local_rank == 0:
         print(f"\n{'='*60}")
